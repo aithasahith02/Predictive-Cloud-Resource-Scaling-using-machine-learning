cb8d23ac7203d9f40c03d02de4046dd86ce31fa7
Sahith Aitha <aithasahith0214@gmail.com>
2024-08-04 21:33:32 -0400

    created a basic script to make an AWS call.

---
+++ tracker_script.sh
@@ -0,0 +1,11 @@
+#!bin/bash/
+######################################
+#
+# Author : Sahith Aitha
+# Date : 8th Aug 2024
+#
+# This is a shell script that retrives basic metrics information from AWS
+######################################
+echo "AWS EC2" :
+
+aws describe-instances
